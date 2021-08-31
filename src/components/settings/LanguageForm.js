862dfbc2727504f64af4f61936ce36d5dd524789
dLeczycki <danielleczycki@gmail.com>
2021-08-31 15:11:53 +0200

    feat: init translations

--- src/components/settings/LanguageForm.js
+++ src/components/settings/LanguageForm.js
@@ -1,18 +1,30 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import {
   Flex, FormLabel, Select,
 } from '@chakra-ui/react';
 
-const LanguageForm = () => (
-  <Flex justifyContent="center" alignItems="center" pt={3}>
-    <FormLabel htmlFor="language" mb="0">
-      Language
-    </FormLabel>
-    <Select>
-      <option value="en">English</option>
-      <option value="pl">Polski</option>
-    </Select>
-  </Flex>
-);
+const LanguageForm = () => {
+  const [language, setLanguage] = useState('en-US');
+  const { t, i18n } = useTranslation();
+
+  const handleChange = (event) => {
+    setLanguage(event.target.value);
+    i18n.changeLanguage(event.target.value);
+  };
+
+  return (
+    <Flex justifyContent="center" alignItems="center" pt={3}>
+      <FormLabel htmlFor="language" mb="0">
+        {t('language')}
+      </FormLabel>
+      <Select value={language} onChange={handleChange}>
+        <option value="en-US">English</option>
+        <option value="pl-PL">Polski</option>
+      </Select>
+    </Flex>
+  );
+};
 
 export default LanguageForm;
--- src/layout/Page/Page.js
+++ src/layout/Page/Page.js
@@ -1,21 +1,36 @@
-import { GridItem } from '@chakra-ui/react';
-import React from 'react';
+import React, { Suspense } from 'react';
+import { GridItem, Spinner, Flex } from '@chakra-ui/react';
 import { Switch, Route } from 'react-router-dom';
 import CVEBrowserPage from '../../pages/CVEBrowserPage';
 import BulletinGeneratorPage from '../../pages/BulletinGeneratorPage';
 import CVSSCalculatorPage from '../../pages/CVSSCalculatorPage';
 import SettingsPage from '../../pages/SettingsPage';
 
-const Page = (props) => (
-  <GridItem as="main" {...props} p={2}>
-    <Switch>
-      <Route component={CVEBrowserPage} path="/" exact />
-      <Route component={CVEBrowserPage} path="/cve-browser" />
-      <Route component={BulletinGeneratorPage} path="/bulletin-generator" />
-      <Route component={CVSSCalculatorPage} path="/cvss-calculator" />
-      <Route component={SettingsPage} path="/settings" />
-    </Switch>
-  </GridItem>
-);
+const Page = (props) => {
+  const spinner = (
+    <Flex height="100%" justifyContent="center" alignItems="center">
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    </Flex>
+  );
+  return (
+    <GridItem as="main" {...props} p={2}>
+      <Suspense fallback={spinner}>
+        <Switch>
+          <Route component={CVEBrowserPage} path="/" exact />
+          <Route component={CVEBrowserPage} path="/cve-browser" />
+          <Route component={BulletinGeneratorPage} path="/bulletin-generator" />
+          <Route component={CVSSCalculatorPage} path="/cvss-calculator" />
+          <Route component={SettingsPage} path="/settings" />
+        </Switch>
+      </Suspense>
+    </GridItem>
+  );
+};
 
 export default Page;
